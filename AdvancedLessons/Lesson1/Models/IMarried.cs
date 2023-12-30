7a78e0c4baee5e1c6352f74a3c05e8d5e6578f32
Demetrius81 <shizimaniak@gmail.com>
2023-12-30 18:09:13 +0300

    feat: hw is ready

--- AdvancedLessons/Lesson1/Models/IMarried.cs
+++ AdvancedLessons/Lesson1/Models/IMarried.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Lesson1.Models;
 
-namespace Lesson3.Models;
 internal interface IMarried
 {
     public FamilyMember Spouse { get; set; }
