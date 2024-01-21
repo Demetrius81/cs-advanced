03a21fad3bc7b79f9e62fe58b2dc7dd335bcda6d
Demetrius81 <shizimaniak@gmail.com>
2024-01-21 18:41:10 +0300

    feat: lection in progress

---
+++ AdvancedLessons/Lesson5/Lections/Part1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5.Lections;
+delegate void MyDelegate();
+internal static class Part1
+{
+    public static void SayHello()
+    {
+        Console.WriteLine("Привет, я - делегат!");
+    }
+
+    public static void SayBye()
+    {
+        Console.WriteLine("Пока!");
+    }
+
+    public static void Run(string[] args)
+    {
+        MyDelegate myDelegate = new MyDelegate(SayHello);
+        MyDelegate myDelegate2 = SayHello;
+
+        myDelegate();
+        myDelegate2();
+
+        Console.WriteLine("------------------------------------------------");
+
+        myDelegate += SayBye;
+        myDelegate();
+
+        Console.WriteLine("------------------------------------------------");
+
+        myDelegate = SayHello;
+        myDelegate += SayHello;
+        myDelegate += SayHello;
+        myDelegate2 = SayBye;
+        myDelegate += myDelegate2;
+        myDelegate();
+
+        Console.WriteLine("------------------------------------------------");
+
+        Console.WriteLine($"В списке вызовов делегата число методов = {myDelegate.GetInvocationList().Length}");
+
+        foreach (MyDelegate del in myDelegate.GetInvocationList())
+        {
+            Console.WriteLine($"Вызываем метод: {del.Method}");
+            del();
+        }
+    }
+}
---
+++ AdvancedLessons/Lesson5/Program.cs
@@ -0,0 +1,11 @@
+﻿using Lesson5.Lections;
+
+namespace Lesson5;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Part1.Run(args);
+    }
+}
